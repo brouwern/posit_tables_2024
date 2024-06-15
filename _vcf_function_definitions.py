0afa3648b7b91ba083027d69df17fa91c6fe6a58
Brouwer <NLB24@pitt.edu>
2024-06-14 21:50:09 -0400

    update font sizes

--- _vcf_function_definitions.py
+++ _vcf_function_definitions.py
@@ -1,94 +1,97 @@
-# FUNCTION DEFINITION
-def vcfgz_to_gz(vcf_gz_file, vcf_name = "my_vcf.vcf", csv_file = "my_snps.csv"):
-    '''This function processes a vcf.gz file using the gzip and shutil packages.
-    gzip is used to unpackage the .gz compressed file format.  (shutil provides
-    file handling functions.'''
-    # Processing cell 1
-    import gzip
-    import shutil
+# FUNCTION INITION
 
-    print("converting your vcf.gz file to .vcf.")
-    with gzip.open(vcf_gz_file, 'rb') as f_in:
-        with open(vcf_name, 'wb') as f_out:
-            shutil.copyfileobj(f_in, f_out)
+  
 
-# FUNCTION DEFINITION
-def vcf_to_df(vcf_file,csv_file = "my_snps.csv"):
-    '''This function processes a .vcf using the StringIO, and re packages.
-     StringIO allows the lines of the .vcf file to be loaded into memory.
-     re is used to do some processing on the file, which is then saved as a
-     temporary .txt file.  Loading the .txt file into pandas removes the
-     metadata and provides a pandas dataframe '''
 
-    print("loading necessary packages")
-    # import libraries and modules
-    ## StringIO: tools for working iwth strings
-    from io import StringIO
 
-    ## re: regular expressions (find and replace patterns)
-    import re
+vcf_file = 
+csv_file = "my_snps.csv"
+#'''This function processes a .vcf using the StringIO, and re packages.
+#StringIO allows the lines of the .vcf file to be loaded into memory.
+#re is used to do some processing on the file, which is then saved as a
+#temporary .txt file.  Loading the .txt file into pandas removes the
+#metadata and provides a pandas dataframe '''
 
-    ## pandas for dataframe
-    import pandas as pd
+print("loading necessary Python packages, StringIO, re")
+# import libraries and modules
+## StringIO: tools for working iwth strings
+from io import StringIO
 
-    import shutil
+## re: regular expressions (find and replace patterns)
+import re
 
-    print("Make sure you're working with a .vcf not .vcf.gz file")
-    print("Opening your VCF file")
-    file1 = open(vcf_file, 'r')
+#?
+import shutil
 
-    print("Reading the lines of the file into a python list")
-    vcf_in_list = file1.readlines()
+print("Make sure you're working with a .vcf not .vcf.gz file")
+print("Opening your VCF file...")
+file1 = open(vcf_file, 'r')
 
-    print("Processing the vcf; this may take a minute")
-    # process vcf file
-    ## loop over list
-    for i in range(0,len(vcf_in_list)):
+print("Reading the lines of the file into a python list...")
+vcf_in_list = file1.readlines()
 
-        # get current row
-        row_i = vcf_in_list[i]
+print("Processing the vcf; this may take a minute...")
+# process vcf file
+## loop over list
+for i in range(0,len(vcf_in_list)):
 
-        # turn "##" comments in vcf file into "#" comments for python
-        row_i = re.sub(r"^##", "#", row_i)
+  # get current row
+  row_i = vcf_in_list[i]
 
-        # remove the "#" from infront of row with CHROM
-        ## so it isn't commented out
-        row_i = re.sub(r"^#CHROM", "CHROM", row_i)
+  # turn "##" comments in vcf file into "#" comments for python
+  row_i = re.sub(r"^##", "#", row_i)
 
-        # store cleaned list
-        vcf_in_list[i] = row_i
+  # remove the "#" from infront of row with CHROM
+  ## so it isn't commented out
+  row_i = re.sub(r"^#CHROM", "CHROM", row_i)
 
-    print("Saving the processed vcf file as a temporary .txt file")
-    # create file to store output
-    with open("vcf_temp.txt", 'w') as file:
-            # loop over list
-            for row in vcf_in_list:
+  # store cleaned list
+  vcf_in_list[i] = row_i
 
-                # do something....
-                vcf_in_list = "".join(map(str, row))
+  print("Saving the processed vcf file as a temporary .txt file")
 
-                #store in the file
-                file.write(vcf_in_list)
+# create file to store output
+with open("vcf_temp.txt", 'w') as file:
+# loop over list
+  for row in vcf_in_list:
 
-    print("Reading your vcf data as a Pandas file without metadata")
-    # read in file back in using pandas
-    ## set comment character as "#"
-    ## so it all gets skipped
-    import pandas as pd
-    my_snps = pd.read_csv("vcf_temp.txt",
-                    sep='\t',
-                    lineterminator='\n',
-                    comment = "#")
-    print("Saving your dataframe as a .csv for backup.  Download this from Colab or else it will disappear : (")
-    my_snps.to_csv(csv_file,
+  # do something....
+  vcf_in_list = "".join(map(str, row))
+
+  #store in the file
+  file.write(vcf_in_list)
+
+print("Reading your vcf data as a Polars file without metadata")
+print("THis may take a minute...")
+# read in file back in using polars
+## set comment character as "#"
+## so it all gets skipped
+import polars as pl
+
+my_snps = pl.read_csv("vcf_temp.txt",
+                    separator='\t',
+                    eol_char='\n',
+                    comment_prefix = "#")
+    ## Pandas
+    #import pandas as pd
+    #my_snps = pd.read_csv("vcf_temp.txt",
+    #                sep='\t',
+    #                lineterminator='\n',
+    #                comment = "#")
+    
+print("Saving your dataframe as a .csv for backup.")
+    #my_snps.to_csv(csv_file,
+    #           index=False)
+
+my_snps.write_csv(csv_file,
                index=False)
 
-    print("Done! \n\n If you're not finding your dataframe, make sure defined a variable to hold the output of the function")
-
-    return(my_snps)
+print("Done! \n\n If you're not finding your dataframe, make sure you assigned a variable to hold the output of the function")
 
 
-def phased_to_featurized(df):
+
+
+ phased_to_featurized(df):
     df_shape = df.shape
     df_nrows = df_shape[0]
     df_ncols = df_shape[1]
@@ -112,10 +115,10 @@ def phased_to_featurized(df):
     return(df_working)
 
 
-# def drop_metadata()
+#  drop_metadata()
 
 
-#def keep_biallelic(snp_df):
+# keep_biallelic(snp_df):
 #    import pandas as pd
 #    real_SNPs = ["A","T","C","G"]
 #    onlyATCG_REF = snp_df.REF.isin(real_SNPs)
